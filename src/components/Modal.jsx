fbb6a49837bd24e172ced968fb5705ba37ed6927
Prajapati Shrikrushna R. <Krishnaprajapati757@gmail.com>
2024-06-23 18:49:03 +0530

    added toggle switch snippets

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,9 @@
-// Modal.js (Updated for Login Snippets)
+// Modal.jsx
 
 import React, { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
-import StringToReactComponent from 'string-to-react-component';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import StringToReactComponent from 'string-to-react-component';
 
 function Modal({ showModal, onClose, jsxCode, cssCode }) {
   const [copyStatus, setCopyStatus] = useState('');
@@ -23,33 +23,31 @@ function Modal({ showModal, onClose, jsxCode, cssCode }) {
         </button>
 
         <div className="p-10 flex justify-center items-center">
-          <StringToReactComponent>
-            {`(props) => (${jsxCode})`}
-          </StringToReactComponent>
-        </div>
-
-        <div className="flex flex-col items-center gap-5">
-          <div className="bg-gray-800 text-white rounded-lg w-full p-2">
-            <div className="flex items-center justify-between">
-              <div className="bg-green-500 p-2">JSX</div>
+          <div className="flex flex-col items-center gap-5">
+            <h3 className="text-lg font-bold mb-4">JSX Code</h3>
+            <div className="bg-gray-800 text-white rounded-lg w-full p-2">
               <CopyToClipboard text={jsxCode} onCopy={() => handleCopy(jsxCode)}>
                 <button className="bg-white text-black p-2 px-4 rounded-lg">
                   Copy to clipboard
                 </button>
               </CopyToClipboard>
+              <div>
+                <StringToReactComponent>
+                  {`(props) => (${jsxCode})`}
+                </StringToReactComponent>
+              </div>
             </div>
-            <pre className="overflow-x-auto p-4">{jsxCode}</pre>
           </div>
-          <div className="bg-gray-800 text-white rounded-lg w-full p-2">
-            <div className="flex items-center justify-between">
-              <div className="bg-green-500 p-2">CSS</div>
+          <div className="flex flex-col items-center gap-5 mt-6">
+            <h3 className="text-lg font-bold mb-4">CSS Code</h3>
+            <div className="bg-gray-800 text-white rounded-lg w-full p-2">
               <CopyToClipboard text={cssCode} onCopy={() => handleCopy(cssCode)}>
                 <button className="bg-white text-black p-2 px-4 rounded-lg">
                   Copy to clipboard
                 </button>
               </CopyToClipboard>
+              <pre className="overflow-x-auto p-4">{cssCode}</pre>
             </div>
-            <pre className="overflow-x-auto p-4">{cssCode}</pre>
           </div>
         </div>
 
