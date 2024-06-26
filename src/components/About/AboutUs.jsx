8df9e65f489bf61f65c26ff6ea6e8a7f4560bd74
Prem Kolte <premkolte26@gmail.com>
2024-06-26 13:32:11 +0530

    add new about page

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -1,55 +1,60 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import Navbar from "../Navbar"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Navbar from "../Navbar";
 
 const About = () => {
   return (
     <>
-      <Navbar />
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className="container mx-auto mt-20 px-4 md:px-8"
-      >
-        <div className="bg-white rounded-lg shadow-lg p-8">
-          <h1 className="text-3xl font-bold mb-4">About Us</h1>
-          <p className="text-lg text-gray-700 mb-4">
-            AnimateHub is your ultimate resource for learning and exploring animation techniques in
-            web development. Whether you're a beginner or an expert, our curated collection of
-            snippets and tutorials will help you bring your web projects to life.
+    <Navbar/>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="relative flex items-center justify-center min-h-screen"
+    >
+      {/* Background Overlay */}
+      <div
+        className="absolute inset-0 bg-white-900 opacity-50"
+        style={{ zIndex: -1 }}
+      ></div>
+
+      {/* Content */}
+      <div className="relative z-10 container mx-auto px-4 md:px-8 text-white">
+        <div className="bg-indigo-600 bg-opacity-90 rounded-lg shadow-lg p-8 md:p-12 lg:w-2/3 xl:w-1/2 mx-auto mt-7">
+          <h1 className="text-3xl font-bold mb-3 ">About Us</h1>
+          <p className="text-lg mb-4">
+            AnimateHub is your ultimate resource for learning and exploring animation techniques in web development. Whether you're a beginner or an expert, our curated collection of snippets and tutorials will help you bring your web projects to life.
           </p>
-          <p className="text-lg text-gray-700 mb-4">
-            Our mission is to inspire creativity and innovation through animation. Join our community
-            and start creating stunning animations today!
+          <p className="text-lg mb-4">
+            Our mission is to inspire creativity and innovation through animation. Join our community and start creating stunning animations today!
           </p>
-          <p className="text-lg text-gray-700 mb-4">
+          <p className="text-lg mb-4">
             Connect with us on social media:
-            <ul className="list-disc pl-8">
-              <li>
-                <a href="https://twitter.com/animatehub" className="text-blue-500 hover:underline">
-                  Twitter
-                </a>
-              </li>
-              <li>
-                <a href="https://facebook.com/animatehub" className="text-blue-500 hover:underline">
-                  Facebook
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://linkedin.com/company/animatehub"
-                  className="text-blue-500 hover:underline"
-                >
-                  LinkedIn
-                </a>
-              </li>
-            </ul>
           </p>
+          <ul className="list-none flex space-x-6">
+            <li>
+              <a href="https://twitter.com/animatehub" className="text-indigo-250 hover:underline">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+            </li>
+            <li>
+              <a href="https://facebook.com/animatehub" className="text-indigo-250 hover:underline">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/company/animatehub" className="text-indigo-250 hover:underline">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              </a>
+            </li>
+          </ul>
         </div>
-      </motion.div>
+      </div>
+    </motion.div>
     </>
   );
-};
+}
 
 export default About;
