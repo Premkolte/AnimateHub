d56e299bab0ffb51dfdf0a4134c99f9e56bc3891
Prajapati Shrikrushna R. <Krishnaprajapati757@gmail.com>
2024-06-26 16:11:50 +0530

    added visit profile in contributors

--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -33,9 +33,11 @@ function SideBar({ activeTab, setActiveTab }) {
         className={`lg:w-64 fixed inset-y-0 left-0 z-40 bg-white shadow-xl transform ${
           isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
         } transition-transform duration-300 ease-in-out lg:translate-x-0 lg:relative lg:inset-0 lg:z-auto overflow-y-auto`}
-        style={{ maxHeight: '100vh', paddingTop: '2rem' }} // Adjusted padding top
+        style={{ maxHeight: '100vh', paddingTop: '2rem' }}
       >
-        <h1 className="text-3xl font-bold text-center" onClick={() => navigate('/')}>AnimateHub</h1>
+        <h1 className="text-3xl font-bold text-center cursor-pointer" onClick={() => navigate('/')}>
+          AnimateHub
+        </h1>
         <div className="p-4 h-full flex flex-col justify-between">
           <div className="space-y-2 mb-4 pb-4">
             {Buttons.map((button, index) => (
@@ -45,20 +47,20 @@ function SideBar({ activeTab, setActiveTab }) {
                   activeTab === index
                     ? 'bg-blue-500 text-white'
                     : 'bg-gray-200 text-gray-800 hover:bg-blue-500 hover:text-white'
-                } py-2 px-4 rounded-md w-full text-lg text-left focus:outline-none`}
+                } py-2 px-4 rounded-md w-full text-lg text-left focus:outline-none transition-colors duration-300`}
                 onClick={() => handleTabClick(index)}
               >
                 {button}
               </button>
             ))}
           </div>
-          <div>
+          <div className="text-center text-sm text-gray-700">
             Type{' '}
-            <kbd className="px-2 py-1.5 text-sm text-gray-900 bg-gray-100 border border-gray-300 rounded-lg">
+            <kbd className="px-2 py-1.5 text-gray-900 bg-gray-100 border border-gray-300 rounded-lg">
               cmd
             </kbd>
             +
-            <kbd className="px-2 py-1.5 text-sm text-gray-900 bg-gray-100 border border-gray-300 rounded-lg">
+            <kbd className="px-2 py-1.5 text-gray-900 bg-gray-100 border border-gray-300 rounded-lg">
               K
             </kbd>{' '}
             for command palette
