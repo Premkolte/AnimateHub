cc25c83e2ef1bf39afdcee349ed4364bf6eb943b
Mehak <mehakmattoo7@gmail.com>
2024-07-17 15:16:17 +0530

    added tab group component

--- src/components/SnippetComponents/TabGroup.jsx
+++ src/components/SnippetComponents/TabGroup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal from "../Modal";
 import StringToReactComponent from "string-to-react-component";
-import { Tab } from "./Snippets/Tab";
+import { tabGroupSnippets } from "./Snippets/Tab";
 
 const TabGroup = () => {
   const [showModal, setShowModal] = useState(false);
@@ -16,13 +16,14 @@ const TabGroup = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-      {Tab.map((snippet, index) => (
+      {tabGroupSnippets.map((snippet, index) => (
         <div key={index} className="p-8 bg-white rounded-lg shadow-lg">
           <h2 className="text-xl font-bold mb-4">{snippet.title}</h2>
           <StringToReactComponent>{snippet.jsxCode}</StringToReactComponent>
+          <style>{snippet.cssCode}</style>
           <div className="mt-4 flex justify-end">
             <button
-              className="text-white text-md py-3 px-2 rounded-lg shadow-md bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 hover:shadow-xl focus:outline-none"
+              className="text-white text-md py-2 px-4 rounded-lg shadow-md bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 hover:shadow-xl focus:outline-none"
               onClick={() => handleShowModal(snippet.jsxCode, snippet.cssCode)}
             >
               Show Code
