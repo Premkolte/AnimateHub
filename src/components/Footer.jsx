48169fa85b8ac1c1791ea93889196590d43e8abe
divyanshii10 <149924007+divyanshii10@users.noreply.github.com>
2025-01-12 16:10:39 +0530

    added social-media-icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,27 @@ const Footer = () => {
       <p className="text-lg">
         &copy; {year} Animate Hub. All rights reserved.
       </p>
+      <div className="flex flex-col items-center mb-4 mt-[-25px]">
+      {/* added social media links */}
+      <div className="flex space-x-4 text-2xl mb-15">
+
+        <a href="https://www.instagram.com/kolte_prem_26/" target="_blank" className="text-pink-500 hover:text-pink-700" aria-label="instagram" rel="noopener noreferrer">
+        <i className="fab fa-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/prem-kolte/" target="_blank" className="text-blue-700 hover:text-blue-900" aria-label="instagram" rel="noopener noreferrer">
+        <i className="fab fa-linkedin-in"></i>
+        </a>
+
+        <a href="https://x.com/PremKolte26?mx=2" target="_blank" className="text-blue-400 hover:text-blue-600" aria-label="instagram" rel="noopener noreferrer">
+        <i className="fab fa-twitter"></i>
+        </a>
+ 
+        <a href="/" target="_blank" className="text-purple-600 hover:text-purple-800" aria-label="instagram" rel="noopener noreferrer">
+        <i className="fas fa-globe"></i>
+        </a>
+      </div>
+      </div>
     </footer>
   );
 };
