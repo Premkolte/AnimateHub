8eafdbe990d460b4cc99adcd7b53497e58e771b7
Prem Kolte <premkolte26@gmail.com>
2025-03-07 16:24:45 +0530

    good

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from 'next-themes';
 import HomePage from "./components/Home/HomePage";
 import AboutUs from "./components/About/AboutUs";
 import ContactUs from "./components/Contact/ContactUs";
@@ -14,29 +15,29 @@ import SignupPage from "./components/Auth/SignUp";
 
 function App() {
   return (
-    <Router>
-      <AnimatedCursor
-        innerSize={12}
-        outerSize={25}
-        outerAlpha={0.4}
-        innerScale={1}
-        outerScale={2}
-        color="194, 198, 204"
-      />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/explore" element={<Dashboard />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/contributors" element={<Contributors />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/templates/*" element={<TemplatesRoutes />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/SignUp" element={<SignupPage/>} />
-
-
-      </Routes>
-    </Router>
+    <ThemeProvider attribute="class">
+      <Router>
+        <AnimatedCursor
+          innerSize={12}
+          outerSize={25}
+          outerAlpha={0.4}
+          innerScale={1}
+          outerScale={2}
+          color="194, 198, 204"
+        />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/explore" element={<Dashboard />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/contributors" element={<Contributors />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/templates/*" element={<TemplatesRoutes />} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/SignUp" element={<SignupPage/>} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
