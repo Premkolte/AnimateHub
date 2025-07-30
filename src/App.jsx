d999e3b58db59b027b74e01c9fa6663a60f55eed
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-07-30 14:39:10 +0530

    initial code for light and dark mode theme added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
 import HomePage from "./components/Home/HomePage";
@@ -13,6 +13,21 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import TemplatesRoutes from "./components/Templates/TemplatesRoutes";
 import LoginPage from "./components/Auth/login";
 import SignupPage from "./components/Auth/SignUp";
+import Navbar from "./components/Home/Navbar";
+import Footer from "./components/Footer";
+import Chatbot from "./components/Home/Chatbot";
+
+// Layout component that includes Navbar, Footer and an Outlet
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Chatbot />
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
@@ -26,17 +41,24 @@ function App() {
           outerScale={2}
           color="194, 198, 204"
         />
+
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/explore" element={<Dashboard />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/contributors" element={<Contributors />} />
-          <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/templates/*" element={<TemplatesRoutes />} />
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/SignUp" element={<SignupPage/>} />
+          {/* Wrap all primary pages with Layout and use Outlet inside */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/explore" element={<Dashboard />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/contributors" element={<Contributors />} />
+            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/templates/*" element={<TemplatesRoutes />} />
+          </Route>
+
+          {/* Auth pages don't use layout (optional) */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/SignUp" element={<SignupPage />} />
         </Routes>
+
         <Analytics />
       </Router>
     </ThemeProvider>
