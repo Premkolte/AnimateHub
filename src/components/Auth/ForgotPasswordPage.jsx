fe3d1f573b763a9ddda7b7f7c39dd6f197d26b79
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-09 20:12:46 +0530

    forgot password UI improved

--- src/components/Auth/ForgotPasswordPage.jsx
+++ src/components/Auth/ForgotPasswordPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useSignIn, useClerk } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const ForgotPasswordPage = () => {
   const { signIn, isLoaded } = useSignIn();
@@ -11,6 +13,7 @@ const ForgotPasswordPage = () => {
   const [code, setCode] = useState("");
   const [password, setPassword] = useState("");
   const [isCodeSent, setIsCodeSent] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [status, setStatus] = useState("");
 
   // Sign out any existing session on load
@@ -59,66 +62,95 @@ const ForgotPasswordPage = () => {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white p-6">
-      <div className="bg-slate-900 p-8 rounded-lg shadow-lg max-w-sm w-full">
-        <button className="text-sm mb-2 text-left" onClick={() => navigate(-1)}>← Back</button>
-        <h1 className="text-2xl font-bold mb-4 text-center">
-          {isCodeSent ? "Reset Password" : "Forgot Password"}
-        </h1>
-
-        {!isCodeSent ? (
-          <form onSubmit={handleSendCode} className="space-y-4">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 bg-gray-800 text-white rounded"
-            />
-            <button
-              type="submit"
-              className="w-full bg-indigo-600 hover:bg-indigo-700 p-3 rounded-lg font-semibold"
-            >
-              Send Code
-            </button>
-          </form>
-        ) : (
-          <form onSubmit={handleResetPassword} className="space-y-4">
-            <input
-              type="text"
-              placeholder="Enter verification code"
-              value={code}
-              required
-              onChange={(e) => setCode(e.target.value)}
-              className="w-full p-2 bg-gray-800 text-white rounded"
-            />
-            <input
-              type="password"
-              placeholder="Enter new password"
-              value={password}
-              required
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 bg-gray-800 text-white rounded"
-            />
-            <button
-              type="submit"
-              className="w-full bg-green-600 hover:bg-green-700 p-3 rounded-lg font-semibold"
-            >
-              Reset Password
-            </button>
-          </form>
-        )}
-
-        {status && <p className="mt-4 text-sm text-yellow-400 text-center">{status}</p>}
-        <p
-          onClick={() => navigate("/sign-in")}
-          className="mt-4 text-sm text-indigo-400 cursor-pointer text-center"
+    <div className="w-full flex flex-col items-center justify-center min-h-screen bg-white dark:bg-secondary-900 text-secondary-900 dark:text-white p-6 space-y-16 py-24">
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-primary-50 dark:bg-secondary-800 text-secondary-900 dark:text-white border border-primary-200 dark:border-secondary-700 p-8 rounded-lg shadow-sm max-w-sm w-full"
         >
-          Back to Login
-        </p>
-      </div>
+          <h1 className="text-3xl font-extrabold mb-6 text-center">
+            {isCodeSent ? "Reset Password" : "Forgot Password"}
+          </h1>
+
+          {!isCodeSent ? (
+            <form onSubmit={handleSendCode} className="space-y-4">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium mb-1">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="mt-1 w-full p-2 border border-primary-100 dark:border-accent-600 dark:bg-gray-800  rounded focus:outline-none focus:ring-2 focus:ring-primary-500 dark:focus:ring-accent-500"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-primary-600 hover:bg-primary-700 dark:bg-accent-600 dark:hover:bg-accent-700 text-white px-6 py-3 rounded-full text-lg font-semibold shadow-lg transition-colors duration-200"
+              >
+                Send Code
+              </button>
+            </form>
+          ) : (
+            <form onSubmit={handleResetPassword} className="space-y-4">
+              <div>
+                <label htmlFor="code" className="block text-sm font-medium mb-1">
+                  Verification Code
+                </label>
+                <input
+                  type="text"
+                  id="code"
+                  className="mt-1 w-full p-2 border border-primary-100 dark:border-accent-600 dark:bg-gray-800 text-white rounded focus:outline-none focus:ring-2 focus:ring-primary-500 dark:focus:ring-accent-500"
+                  value={code}
+                  onChange={(e) => setCode(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="new-password" className="block text-sm font-medium mb-1">
+                  New Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={passwordVisible ? "text" : "password"}
+                    id="new-password"
+                    className="mt-1 w-full p-2 border border-primary-100 dark:border-accent-600 dark:bg-gray-800 text-white rounded focus:outline-none focus:ring-2 focus:ring-primary-500 dark:focus:ring-accent-500"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <button type="button" onClick={togglePasswordVisibility} className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-primary-600 dark:hover:text-accent-500 transition-colors duration-200">
+                    {passwordVisible ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                  </button>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-primary-600 hover:bg-primary-700 dark:bg-accent-600 dark:hover:bg-accent-700 text-white px-6 py-3 rounded-full text-lg font-semibold shadow-lg transition-colors duration-200"
+              >
+                Reset Password
+              </button>
+            </form>
+          )}
+
+          {status && <p className="mt-4 text-sm text-yellow-400 text-center">{status}</p>}
+          <p
+            onClick={() => navigate("/sign-in")}
+            className="mt-4 text-sm text-primary-600 dark:text-accent-500 cursor-pointer text-center hover:underline"
+          >
+            Back to Login
+          </p>
+        </motion.div>
+
     </div>
   );
 };
