931d2cc093eaa0d9c6cf383856666152fedb162c
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-14 22:53:35 +0530

    Fixed the /#/{endpoint} issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import { Analytics } from '@vercel/analytics/react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
