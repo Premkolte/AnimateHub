5523064edcbb25c26a5f06d696236587476a33dd
swayam agarwal <you@example.com>
2025-08-27 16:56:48 +0530

    Added Controllers for blog

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -57,3 +57,40 @@ export const createBlog = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(201, "Blog created successfully", newBlog));
 });
+
+export const getBlogById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const blog = await Blog.findById(id).populate("author", "name email avatarUrl ");
+
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Blog fetched successfully", blog));
+});
+
+export const deleteBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const blog = await Blog.findById(id);
+
+  if (!blog) {
+    throw new ApiError(404, "Blog not found");
+  }
+
+  // Author check (sirf jisne create kiya, wahi delete kar sake)
+  if (blog.author.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to delete this blog");
+  }
+
+  await blog.deleteOne();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Blog deleted successfully"));
+});
+
+
--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { createBlog } from "../controllers/blog.controller.js";
+import { createBlog,getBlogById,deleteBlog } from "../controllers/blog.controller.js";
 import upload from "../utils/fileUpload.js";
 const router=Router()
 
@@ -8,4 +8,5 @@ const router=Router()
 
 
 router.post("/create",authMiddleware,upload.single("blog-image"),createBlog)
+router.get("/:id",authMiddleware,getBlogById).delete(authMiddleware,deleteBlog)
 export default router
