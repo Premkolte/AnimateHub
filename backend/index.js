9ab5fb176b79ae2beac0bcfc4820032e76218fa9
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-30 15:35:58 +0530

    Removed the hardcoded PORT value

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@ import "dotenv/config" // load the env file
 import connectToDatabase from "./src/utils/db.js"
 import { app } from "./src/app.js"
 
+const PORT = process.env.PORT || 5000;
 connectToDatabase().then(() => {
-    app.listen(5000, () => {
-        console.log(`SERVER running at port: 5000`)
+    app.listen(PORT, () => {
+        console.log(`SERVER running at port: ${PORT}`)
     })
 }).catch(() => {
     console.log("Failed to connect to the database.")
