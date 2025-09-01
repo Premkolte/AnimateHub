84e0da30cee4047083c6df1f936f94ab99f8792b
swayam agarwal <you@example.com>
2025-09-01 15:00:12 +0530

    Added Missing props

--- src/Pages/Favorites/withFavoriteButton.jsx
+++ src/Pages/Favorites/withFavoriteButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FavoriteButton from './FavoriteButton';
+import PropTypes from 'prop-types';
 
 // Higher-order component to add favorite functionality to snippet cards
 const withFavoriteButton = (WrappedComponent, snippetType) => {
@@ -26,9 +27,22 @@ const withFavoriteButton = (WrappedComponent, snippetType) => {
       
       return <WrappedComponent {...otherProps} items={itemsWithFavorites} />;
     }
+     WithFavoriteButtonComponent.propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        label: PropTypes.string,
+        jsxCode: PropTypes.string,
+        cssCode: PropTypes.string,
+        favoriteButton: PropTypes.node, // extra field injected by HOC
+      })
+    ),
+  };
     
     return <WrappedComponent {...props} />;
   };
 };
+
+
 
 export default withFavoriteButton;
