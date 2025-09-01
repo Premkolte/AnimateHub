26af4075cc9ae6939265e3a97cecd40b79e95241
swayam agarwal <you@example.com>
2025-09-01 14:49:56 +0530

    Added Comment Schema and setup Router for it

---
+++ backend/src/controllers/comment.controller.js
@@ -0,0 +1,4 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import Comment from "../models/comment.model.js";
---
+++ backend/src/routes/comment.route.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import authMiddleware from "../middlewares/auth.middleware.js";
+const router = Router()
+
+
+
+export default router
