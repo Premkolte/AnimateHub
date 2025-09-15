dfd616dc587b3ae1a871c4dbdf99d17bbafb1234
Sashreek <iamsashreeek@gmail.com>
2025-09-16 01:26:59 +0530

    Level 3 improvements: clean routing, UX enhancements, deploy-ready

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Analytics } from "@vercel/analytics/react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
-// Import Layout --------------------------
+// Layout & UI components
 import Navbar from "./components/layout/Navbar";
 import Chatbot from "./components/layout/Chatbot";
 import Footer from "./components/layout/Footer";
@@ -17,11 +17,11 @@ import BackToTop from "./components/UI/BackToTop";
 import { Toaster } from "react-hot-toast";
 import ScrollToTop from "./components/ScrollToTop";
 
-// Middleware -------------------------------
+// Middleware
 import ProtectedRoute from "./Pages/Auth/ProtectedRoute";
 import AppProvider from "./providers/AppProvider";
 
-// Import pages ---------------------------
+// Pages
 import HomePage from "./Pages/Home/HomePage";
 import AboutUs from "./Pages/About/AboutUs";
 import ContactUs from "./Pages/Contact/ContactUs";
@@ -48,15 +48,6 @@ import InstallationPage from "./Pages/Components/staticPages/InstallationPage";
 
 import Playground from "./Pages/Playground/Code_Playground";
 import FramerPlayground from "./Pages/Playground/FramerPlayground";
-
-
-import ContributorGuide from "./Pages/Contributors/ContributorGuide";
-import ProfilePage from "./Pages/Profile/ProfilePage";
-import PalettesPage from "./Pages/Profile/PalettesPage";
-
-
-
-import Resourcehub from "./Pages/Profile/Resourcehub";
 import ColorGradientPlayground from "./Pages/Playground/ColorGradientPlayground";
 import TailwindPlayground from "./Pages/Playground/TailwindPlayground";
 import SVGPlayground from "./Pages/Playground/SVGPlayground";
@@ -64,92 +55,100 @@ import FontPlayground from "./Pages/Playground/FontPlayground";
 import FlexboxPlayground from "./Pages/Playground/FlexboxPlayground";
 import GridPlayground from "./Pages/Playground/GridPlayground";
 
+import ContributorGuide from "./Pages/Contributors/ContributorGuide";
+import ProfilePage from "./Pages/Profile/ProfilePage";
+import PalettesPage from "./Pages/Profile/PalettesPage";
+import Resourcehub from "./Pages/Profile/Resourcehub";
 
-// Layout component that includes Navbar, Footer and an Outlet
+// -----------------------------------
+// Layout component that wraps all pages
+// -----------------------------------
 function Layout() {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen">
+      <main className="min-h-screen transition-all duration-300 ease-in-out">
         <Outlet />
       </main>
       <Chatbot />
       <Footer />
       <BackToTop />
-      <Toaster />
+      <Toaster position="top-right" reverseOrder={false} />
     </>
   );
 }
 
+// -----------------------------------
+// App Component - Level 3 Improvements
+// -----------------------------------
 function App() {
   return (
     <AppProvider>
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        {/* UX Enhancements */}
         <SplashCursor />
         <ScrollToTop />
 
         <Routes>
-          {/* Wrap all primary pages with Layout and use Outlet inside */}
+          {/* Primary layout wrapper */}
           <Route element={<Layout />}>
+            {/* Home & Info Pages */}
             <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<HomePage />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />} />
+
+            {/* Components & Playground Routes */}
             <Route path="/components" element={<ComponentsPage />}>
               <Route path="introduction" element={<IntroductionPage />} />
               <Route path="installation" element={<InstallationPage />} />
             </Route>
-            <Route path="/explore" element={<Dashboard />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route
-              path="/animationplayground"
-              element={<AnimationPlayground />}
-            />
-            <Route path="/challenge" element={<ChallengeMode />} />
-            <Route path="/blogs" element={<Blog />} />
+
             <Route path="/playground" element={<Playground />} />
             <Route path="/framerplayground" element={<FramerPlayground />} />
-
             <Route path="/ColorGradientPlayground" element={<ColorGradientPlayground />} />
             <Route path="/TailwindPlayground" element={<TailwindPlayground />} />
             <Route path="/SVGPlayground" element={<SVGPlayground />} />
+            <Route path="/FontPlayground" element={<FontPlayground />} />
+            <Route path="/flexboxPlayground" element={<FlexboxPlayground />} />
+            <Route path="/gridPlayground" element={<GridPlayground />} />
+            <Route path="/animationplayground" element={<AnimationPlayground />} />
 
-
-            <Route path="/FontPlayground" element={<FontPlayground/>}/>
-            <Route path="/flexboxPlayground" element={<FlexboxPlayground/>}/>
-            <Route path="/gridPlayground" element={<GridPlayground/>}/>
-
-
-
-
-
-
-
-
-
+            {/* Dashboard & Challenge Mode */}
+            <Route path="/explore" element={<Dashboard />} />
+            <Route path="/challenge" element={<ChallengeMode />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
-            <Route path="/contact" element={<ContactUs />} />
+
+            {/* Contributor Pages */}
             <Route path="/contributors" element={<Contributors />} />
-            <Route path="/favorites" element={<ProtectedRoute><FavoritesPage /></ProtectedRoute>} />
-            <Route path="/payment" element={<ProtectedRoute><PaymentPage /></ProtectedRoute>} />
-            <Route path="/templates/*" element={<TemplatesRoutes />} />
-            <Route path="/my-snippets" element={<MySnippet />} />
             <Route path="/contributor-guide" element={<ContributorGuide />} />
 
-            {/* Auth Routes */}
+            {/* Templates & Payment */}
+            <Route path="/templates/*" element={<TemplatesRoutes />} />
+            <Route path="/payment" element={<ProtectedRoute><PaymentPage /></ProtectedRoute>} />
+            <Route path="/favorites" element={<ProtectedRoute><FavoritesPage /></ProtectedRoute>} />
+            <Route path="/my-snippets" element={<MySnippet />} />
+
+            {/* Auth */}
             <Route path="/sign-in" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/verify-email/:token" element={<VerifyEmail />} />
             <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
 
-            {/* Profile Routes */}
+            {/* Profile */}
             <Route path="/profile/:username" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
             <Route path="/palettes" element={<PalettesPage />} />
             <Route path="/resourcehub" element={<Resourcehub />} />
+
+            {/* Fallback */}
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
 
+        {/* Analytics & Performance Monitoring */}
         <Analytics />
       </Router>
+
       <SpeedInsights />
     </AppProvider>
   );
